bc0a548abd07efd0903bcb69cef8e48c200deb40
Simon Ayzman <simonayzman@gmail.com>
2014-04-06 03:38:42 -0400

    updated instagram

--- instagramTestApp.py
+++ instagramTestApp.py
@@ -1,8 +1,12 @@
-CLIENT ID	f4c597a88f114c0ba8eb949d54336a58
-CLIENT SECRET	c4b0f208cda64720b534fc14b126007b
+instagram_client_key = 'f4c597a88f114c0ba8eb949d54336a58'
+instagram_client_secret = 'c4b0f208cda64720b534fc14b126007b'
 
-instagram_client_key = 'T0foLprojTuyETudVyaUPYUeP'
-instagram_client_secret = 'nmY0z8LClJr1vuTUCnIORsiFtaTzW6ckVJDvj60ATMXryhlQlw'
+api = InstagramAPI(client_id='YOUR_CLIENT_ID', client_secret='YOUR_CLIENT_SECRET')
+popular_media = api.media_popular(count=20)
+for media in popular_media:
+    print media.images['standard_resolution'].url
 
-twitter_access_token = '719330150-eM3GGu06smiyVBu6qp09acCP1YTmULr50ftvytAq'
-twitter_access_token_secret = 'atlE2CeabrB49A9ld0meSZinWPBSl9aslW5KoTwBHBX3H'
+
+api.create_subscription(object='location', object_id='1257285', aspect='media', callback_url='http://mydomain.com/hook/instagram')
+# Subscribe to all media in a geographic area
+api.create_subscription(object='geography', lat=35.657872, lng=139.70232, radius=1000, aspect='media', callback_url='http://mydomain.com/hook/instagram')
